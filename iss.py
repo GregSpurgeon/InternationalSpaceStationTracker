dac6808ea442eff7e489dde06d42387843802f7a
Greg Spurgeon <spurgeon.greg11@gmail.com>
2020-09-25 16:46:14 -0400

    created functions for Part A and Part B. Both functions are working as intended

--- iss.py
+++ iss.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
+import requests
 
 __author__ = '???'
 
 
+def get_astronaunts_info():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    raw_data = r.json()
+    print(f"There are currently {raw_data['number']} astronaunts in space")
+    people = raw_data['people']
+    for person in people:
+        for _ in person:
+            print(person['name'] + " is on board " + person['craft'])
+            break
+
+
+def get_geographic_coordinates():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    raw_data = r.json()
+    print("timestamp =", raw_data['timestamp'])
+    iss_position = raw_data['iss_position']
+    for k, v in iss_position.items():
+        print(f"{k} is {v}")
+
+
 def main():
-    pass
+    get_astronaunts_info()
+    get_geographic_coordinates()
 
 
 if __name__ == '__main__':
